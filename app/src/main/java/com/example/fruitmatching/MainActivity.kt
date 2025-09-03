956c3bd80279c0693f5fdcdf3eedddcc57bfff2e
Kropka8o <kropka.koniec.swiata@gmail.com>
2025-09-03 15:29:19 +0200

    Initial commit
    Initial commit + basic pages and buttons.

---
+++ app/src/main/java/com/example/fruitmatching/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fruitmatching
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val playButton = findViewById(R.id.playButton) as Button
+        val helpButton = findViewById(R.id.helpButton) as Button
+
+        playButton?.setOnClickListener {
+            val intent = android.content.Intent(this, GameActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/fruitmatching/RulesActivity.kt
@@ -0,0 +1,29 @@
+package com.example.fruitmatching
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RulesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_rules)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun goBack(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
+    }
+}
